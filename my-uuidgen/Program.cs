7ddc83dba76c6b05c5013df67caaa9904862b121
Brian Hart <brian.hart@xylogix.net>
2019-01-19 17:34:34 -0500

    19 Jan 2019 Added resources and implemented --version switch

--- my-uuidgen/Program.cs
+++ my-uuidgen/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using my_uuidgen.Properties;
+using System;
+using System.Reflection;
 
 namespace my_uuidgen
 {
@@ -9,11 +11,21 @@ namespace my_uuidgen
     {
         public static void Main(string[] args)
         {
+            if (args.Length > 0)
+            {
+                if (Resources.VersionSwitch.Equals(args[0]))
+                {
+                    Console.WriteLine(Assembly.GetExecutingAssembly().GetName().Version);
+                    Environment.Exit(0);    /* exit code of zero means success */
+                }
+            }
+
             /* This software has one job in life -- to get a new Globally-Unique Identifier (GUID) and then
              * write it to the standard output and then exit.  This program is meant to replicate the uuidgen.exe
              * utility provided with the Windows SDK, but I wanted to use it in my own batch files, and who the heck
              * wants to download and install the SDK all the time? */
             Console.WriteLine($"{Guid.NewGuid()}");
+            Environment.Exit(0);    /* exit code of zero means success */
         }
     }
 }
