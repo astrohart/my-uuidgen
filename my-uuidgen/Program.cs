a849e13f4b7f7a6b55fff1aa73d7ef0d5ce39a55
Brian Hart <brian.hart@xylogix.net>
2019-01-19 17:13:48 -0500

    19 Jan 2019 Updated the properties to prevent creation of PDB files in Release mode for all platforms

---
+++ my-uuidgen/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace my_uuidgen
+{
+    /// <summary>
+    /// Defines the routines for this software
+    /// </summary>
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            /* This software has one job in life -- to get a new Globally-Unique Identifier (GUID) and then
+             * write it to the standard output and then exit.  This program is meant to replicate the uuidgen.exe
+             * utility provided with the Windows SDK, but I wanted to use it in my own batch files, and who the heck
+             * wants to download and install the SDK all the time? */
+            Console.WriteLine($"{Guid.NewGuid()}");
+        }
+    }
+}
