45c02820a269a5faa45dd58a38025c93f19e33e0
Brian Hart <brian.hart@xylogix.net>
2021-05-27 22:11:25 -0400

    Create FormatType enum

---
+++ my-uuidgen/FormatType.cs
@@ -0,0 +1,36 @@
+﻿namespace my_uuidgen
+{
+    /// <summary>
+    /// Specifies the format of the outputted GUID.
+    /// </summary>
+    public enum FormatType
+    {
+        /// <summary>
+        /// Returns 32 hex digits only.
+        /// </summary>
+        DigitsOnly,
+
+        /// <summary>
+        /// Returns 32 hex digits broken into GUID groups using hyphens.
+        /// </summary>
+        DigitsWithHyphens,
+
+        /// <summary>
+        /// Returns 32 hex digits broken into GUID groups using hyphens,
+        /// surrounded by braces (Registry format).
+        /// </summary>
+        DigitsHyphensAndBraces,
+
+        /// <summary>
+        /// Returns 32 hex digits broken into GUID groups using hyphens,
+        /// surrounded by parentheses.
+        /// </summary>
+        DigitsHyphensAndParentheses,
+
+        /// <summary>
+        /// Four hexadecimal values enclosed in braces, where the fourth value
+        /// is a subset of eight hexadecimal values that is also enclosed in braces.
+        /// </summary>
+        HexGroups
+    }
+}
--- my-uuidgen/Program.cs
+++ my-uuidgen/Program.cs
@@ -12,14 +12,32 @@ namespace my_uuidgen
     public static class Program
     {
         /// <summary>
-        /// Gets or sets a value indicating whether UUIDs are written to the standard output with hex digits A-F in UPPERCASE or not.
+        /// Gets or sets the <see cref="T:my_uuidgen.FormatType" /> value that
+        /// specifies how to format the GUID.
+        /// </summary>
+        public static FormatType FormatType { get; set; } =
+            FormatType.DigitsHyphensAndBraces;
+
+        /// <summary>
+        /// Gets or sets a value indicating whether UUIDs are written to the
+        /// standard output with hex digits A-F in UPPERCASE or not.
         /// </summary>
         private static bool IsUppercase { get; set; }
 
         /// <summary>
+        /// Gets or sets a value that indicates whether we should not place the
+        /// generated GUID text onto the Clipboard after it's been written to
+        /// standard output.
+        /// </summary>
+        private static bool ShouldNotCopy { get; set; }
+
+        /// <summary>
         /// Entry point.
         /// </summary>
-        /// <param name="args">Array of strings containing the command-line arguments passed to the program by the user.</param>
+        /// <param name="args">
+        /// Array of strings containing the command-line arguments passed to the
+        /// program by the user.
+        /// </param>
         [STAThread]
         public static void Main(string[] args)
         {
@@ -27,7 +45,11 @@ namespace my_uuidgen
             {
                 if (Resources.VersionSwitch.Equals(args[0]))
                 {
-                    Console.WriteLine(Assembly.GetExecutingAssembly().GetName().Version);
+                    Console.WriteLine(
+                        Assembly.GetExecutingAssembly()
+                                .GetName()
+                                .Version
+                    );
                     Environment.Exit(0); /* exit code of zero means success */
                 }
                 else
@@ -44,26 +66,23 @@ namespace my_uuidgen
 
             var newGuid = Guid.NewGuid();
 
-            var guidString = IsUppercase ? $"{newGuid.ToString().ToUpperInvariant()}" : $"{newGuid}";
+            var guidString = IsUppercase
+                ? $"{newGuid.ToString().ToUpperInvariant()}"
+                : $"{newGuid}";
 
             Console.WriteLine(guidString);
 
             if (!ShouldNotCopy)
-            {
-                // place the GUID string that we otherwise pump to standard output, also to be on the Clipboard.
-                // This way, this app can also be launched, e.g., from the Tools menu on Visual Studio and then the user
-                // can just do a paste into whatever file they are working on right off the bat.
+
+                // place the GUID string that we otherwise pump to standard
+                // output, also to be on the Clipboard. This way, this app can
+                // also be launched, e.g., from the Tools menu on Visual Studio
+                // and then the user can just do a paste into whatever file they
+                // are working on right off the bat.
 
                 Clipboard.SetText(guidString);
-            }
 
-            Environment.Exit(0);    /* exit code of zero means success */
+            Environment.Exit(0); /* exit code of zero means success */
         }
-
-        /// <summary>
-        /// Gets or sets a value that indicates whether we should not place the generated GUID text onto the Clipboard
-        /// after it's been written to standard output.
-        /// </summary>
-        private static bool ShouldNotCopy { get; set; }
     }
 }
