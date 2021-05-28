0efa6b862e2d3a06b6e25208bc0b0d39cbe5c9a9
Brian Hart <brian.hart@xylogix.net>
2021-05-27 22:19:06 -0400

    Create GetFormatType and GetGuidFormatSpecifier classes

---
+++ my-uuidgen/GetFormatType.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace my_uuidgen
+{
+    /// <summary>
+    /// Gets <see cref="T:my_uuidgen.FormatType"/> values from command-line arguments.
+    /// </summary>
+    public static class GetFormatType
+    {
+        public static FormatType FromSwitch(string argument)
+        {
+            var result = FormatType.DigitsHyphensAndBraces;
+
+            switch (argument)
+            {
+                case "--registry":
+                    return result;
+
+                case "--parens":
+                    result = FormatType.DigitsHyphensAndParentheses;
+                    break;
+
+                case "--hyphensonly":
+                    result = FormatType.DigitsWithHyphens;
+                    break;
+
+                case "--noformat":
+                    result = FormatType.DigitsOnly;
+                    break;
+
+                case "--hexgroups":
+                    result = FormatType.HexGroups;
+                    break;
+            }
+            
+            return result;
+        }
+    }
+}
---
+++ my-uuidgen/GetGuidFormatSpecifier.cs
@@ -0,0 +1,57 @@
+﻿namespace my_uuidgen
+{
+    /// <summary>
+    /// Translates input values into GUID string-formatting format-specifier codes.
+    /// </summary>
+    public static class GetGuidFormatSpecifier
+    {
+        /// <summary>
+        /// Determines the format-specifier for the
+        /// <see
+        ///     cref="M:System.Guid.ToString" />
+        /// method, given one of the
+        /// <see
+        ///     cref="T:my_uuidgen.FormatType" />
+        /// values being passed.
+        /// </summary>
+        /// <param name="type">
+        /// (Required.) The <see cref="T:my_uuidgen.FormatType" /> value to be
+        /// translated into a format-specifier.
+        /// </param>
+        /// <returns>
+        /// String containing a format-specifier to then be fed to the
+        /// <see
+        ///     cref="M:System.Guid.ToString" />
+        /// method.
+        /// </returns>
+        public static string FromFormatType(FormatType type)
+        {
+            var result = "B";
+
+            switch (type)
+            {
+                case FormatType.DigitsWithHyphens:
+                    result = "D";
+                    break;
+
+                case FormatType.DigitsHyphensAndParentheses:
+                    result = "P";
+                    break;
+
+                case FormatType.DigitsHyphensAndBraces:
+                    result = "B";
+                    break;
+
+                case FormatType.HexGroups:
+                    result = "X";
+                    break;
+
+                case FormatType.DigitsOnly:
+                    result = "N";
+                    break;
+            }
+
+            return result;
+        }
+    }
+}
